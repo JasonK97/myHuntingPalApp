1262af66a83057f3cf9ce1dfcc9fafacd5c2c54a
Jason Kent <jasonbkent@gmail.com>
2023-03-27 14:05:28 -0600

    initial commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.pageTitle}>Welcome to My Hunting Pal</Text>
+      <Text style={styles.text}>Elevate your hunting experience.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#262626',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  pageTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#ffffff',
+  },
+  text: {
+    fontSize: 18,
+    color: '#ffffff',
   },
 });
